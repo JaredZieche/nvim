c261270b4b7495f03cf2938d79f922a317d054d7
jaredzieche <jared.zieche@va.gov>
2022-01-21 18:02:59 -0500

    commented lsp keymap section due to errors with on_attach func

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -2,46 +2,46 @@ local nvim_lsp = require("lspconfig")
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(bufnr)
-    local function buf_set_keymap(...)
-        vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
+-- local on_attach = function(bufnr)
+--     local function buf_set_keymap(...)
+--         vim.api.nvim_buf_set_keymap(bufnr, ...)
+--     end
 
-    -- Mappings.
-    local opts = {noremap = true, silent = true}
+--     -- Mappings.
+--     local opts = {noremap = true, silent = true}
 
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    -- buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    buf_set_keymap("n", "<space>wa",
-                   "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-    buf_set_keymap("n", "<space>wr",
-                   "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-    buf_set_keymap("n", "<space>wl",
-                   "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
-                   opts)
-    buf_set_keymap("n", "<space>D",
-                   "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-    buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-    buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>",
-                   opts)
-    buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    buf_set_keymap("n", "<space>e",
-                   "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>",
-                   opts)
-    buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>",
-                   opts)
-    buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>",
-                   opts)
-    buf_set_keymap("n", "<space>q",
-                   "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-    buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>",
-                   opts)
+--     -- See `:help vim.lsp.*` for documentation on any of the below functions
+--     buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+--     buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+--     buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+--     buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+--     -- buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+--     buf_set_keymap("n", "<space>wa",
+--                    "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+--     buf_set_keymap("n", "<space>wr",
+--                    "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+--     buf_set_keymap("n", "<space>wl",
+--                    "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
+--                    opts)
+--     buf_set_keymap("n", "<space>D",
+--                    "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+--     buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+--     buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>",
+--                    opts)
+--     buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+--     buf_set_keymap("n", "<space>e",
+--                    "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>",
+--                    opts)
+--     buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>",
+--                    opts)
+--     buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>",
+--                    opts)
+--     buf_set_keymap("n", "<space>q",
+--                    "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+--     buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>",
+--                    opts)
 
-end
+-- end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
@@ -113,18 +113,18 @@ for _, lsp in ipairs(servers) do
                     url = "https://www.schemastore.org/api/json/catalog.json"
                 },
                 schemas = {
+                    kubernetes = "*.yaml",
                     ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
                     ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
                     ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
                     ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
                     ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-                    ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+                    ["http://json.schemastore.org/ansible-playbook"] = "*ansible/*.{yml,yaml}",
                     ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
                     ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
                     ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
                     ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
-                    ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose.{yml,yaml}",
-                    kubernetes = "/*.yaml"
+                    ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose.{yml,yaml}"
                 },
                 format = {enabled = false},
                 validate = false, -- TODO: conflicts between Kubernetes resources and kustomization.yaml
