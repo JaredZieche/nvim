7b146158b382a0b1e60bb3a7299d11de52086f96
jaredzieche <jared.zieche@va.gov>
2022-05-06 08:57:09 -0400

    updated cmp.lua to fix tab completion on commandline

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -79,26 +79,26 @@ cmp.setup({
             select = false
         },
         ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, {"i", "s"}),
 
         ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, {"i", "s"})
     },
     sources = {
         {name = "nvim_lsp"}, {name = "buffer", keyword_length = 5},
@@ -112,5 +112,6 @@ cmp.setup.cmdline("/", {sources = {{name = "buffer"}}})
 
 -- Use cmdline & path source for ':'.
 cmp.setup.cmdline(":", {
+    mapping = cmp.mapping.preset.cmdline(),
     sources = cmp.config.sources({{name = "path"}}, {{name = "cmdline"}})
 })
